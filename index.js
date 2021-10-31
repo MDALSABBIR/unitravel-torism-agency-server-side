f87e5e8e202ebe1e9dc498a3e59e9bb05f3dd481
MD.ALSABBIR <86665168+MDALSABBIR@users.noreply.github.com>
2021-10-31 21:02:25 +0600

    server site

--- index.js
+++ index.js
@@ -1,17 +1,117 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+// const axios = require("axios").default;
 
 const app = express();
-
-app.use(cors());
-app.use(express.json());
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('hello wow to learn express  hello nodmon from my second node server');
+// Middlewar
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rlmgi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("travel_site");
+        const eventCollection = database.collection("events");
+        const usersCollection = database.collection("users");
+
+        // get api
+        app.get("/events", async (req, res) => {
+            const cursor = eventCollection.find({});
+            const events = await cursor.toArray();
+            res.send(events);
+        });
+
+        //single event
+        app.get('/events/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const event = await eventCollection.findOne(query);
+            res.send(event);
+        })
+
+        // get api
+        app.get("/users", async (req, res) => {
+            const cursor = usersCollection.find({});
+            const events = await cursor.toArray();
+            res.send(events);
+        });
+        // get api for users
+        app.get("/users/:email", async (req, res) => {
+            const cursor = usersCollection.find({ email: req.params.email });
+            const events = await cursor.toArray();
+            res.send(events);
+        });
+
+        // Post Api
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            console.log("hit the api", user);
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+
+        // Get one User
+        app.get("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.findOne(query);
+            console.log("finding one User", result);
+            res.send(result);
+        });
+
+        // Update Api
+        app.put("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            console.log(updatedUser);
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    status: updatedUser.status,
+                },
+            };
+            console.log(updateDoc);
+            const resut = await usersCollection.updateOne(filter, updateDoc, options);
+
+            console.log("Updating User", id);
+            res.json(resut);
+        });
+
+        // Delete Api
+        app.delete("/users/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            console.log("deleteing the User", result);
+
+            res.json(result);
+        });
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Running My Curd Server");
+});
 
 app.listen(port, () => {
-    console.log('Listening to port', port);
+    console.log("Running server On port", port);
 });
+
+
+
